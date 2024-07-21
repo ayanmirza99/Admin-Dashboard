b1537b08e68116eb0f18ce88f039dce612c349a8
ayanmirza99 <125931448+ayanmirza99@users.noreply.github.com>
2024-07-22 00:07:09 +0500

    routes and side-nav-bar developed

---
+++ components/Sidebar.jsx
@@ -0,0 +1,60 @@
+"use client"
+import React, { useState } from 'react'
+import { Nav } from './ui/nav'
+import {
+    ChevronRight,
+    LayoutDashboard,
+    ListOrdered,
+    Settings,
+    UsersRound,
+} from "lucide-react"
+import { Button } from './ui/button'
+import {
+    useWindowWidth,
+} from '@react-hook/window-size'
+
+const Sidebar = () => {
+    const [isCollapsed, setIsCollapsed] = useState(false)
+    const screenWidth = useWindowWidth()
+    const mobileScreen = screenWidth < 768
+
+    return (
+        <div className='relative min-w-[60px] md:min-w-[80px] border-r md:px-6 pb-10 pt-[14rem]'>
+            {!mobileScreen && (
+
+                <div className='absolute right-[-30px] top-7' onClick={() => setIsCollapsed(!isCollapsed)}>
+                    <Button variant="secondary" className="rounded-full p-2 h-16 w-16">
+                        <ChevronRight />
+                    </Button>
+                </div>
+            )}
+            <Nav
+                isCollapsed={mobileScreen ? true : isCollapsed}
+                links={[
+                    {
+                        title: "Dashboard",
+                        href: "/",
+                        icon: LayoutDashboard,
+                    },
+                    {
+                        title: "Users",
+                        href: "/users",
+                        icon: UsersRound,
+                    },
+                    {
+                        title: "Orders",
+                        href: "/orders",
+                        icon: ListOrdered,
+                    },
+                    {
+                        title: "Settings",
+                        href: "/settings",
+                        icon: Settings,
+                    },
+                ]}
+            />
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ components/ui/nav.jsx
@@ -0,0 +1,92 @@
+"use client";
+
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+import { usePathname } from "next/navigation";
+
+// interface NavProps {
+//   isCollapsed: boolean;
+//   links: {
+//     title: string;
+//     label?: string;
+//     icon: LucideIcon;
+//     variant: "default" | "ghost";
+//     href: string;
+//   }[];
+// }
+
+export function Nav({ links, isCollapsed }) {
+  const pathName = usePathname()
+
+  return (
+    <TooltipProvider>
+      <div
+        data-collapsed={isCollapsed}
+        className="group flex flex-col items-center gap-4 py-2 data-[collapsed=true]:py-2"
+      >
+        <nav className="grid gap-6 md:gap-12 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
+          {links.map((link, index) =>
+            isCollapsed ? (
+              <Tooltip key={index} delayDuration={0}>
+                <TooltipTrigger asChild>
+                  <Link
+                    href={link.href}
+                    className={cn(
+                      buttonVariants({ variant: link.href === pathName ? "default" : "ghost", size: "icon" }),
+                      "md:h-12 md:w-12 h-8 w-8",
+                      link.variant === "default" &&
+                      "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                    )}
+                  >
+                    <link.icon className="md:h-8 md:w-8 h-6 w-6" />
+                    <span className="sr-only">{link.title}</span>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent side="right" className="flex items-center gap-4">
+                  {link.title}
+                  {link.label && (
+                    <span className="ml-auto text-muted-foreground">
+                      {link.label}
+                    </span>
+                  )}
+                </TooltipContent>
+              </Tooltip>
+            ) : (
+              <Link
+                key={index}
+                href={link.href}
+                className={cn(
+                  buttonVariants({ variant: link.href === pathName ? "default" : "ghost", size: "lg" }),
+                  link.variant === "default" &&
+                  "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
+                  "justify-start"
+                )}
+              >
+                <link.icon className="mr-2 h-7 w-7" />
+                {link.title}
+                {link.label && (
+                  <span
+                    className={cn(
+                      "ml-auto",
+                      link.variant === "default" &&
+                      "text-background dark:text-white"
+                    )}
+                  >
+                    {link.label}
+                  </span>
+                )}
+              </Link>
+            )
+          )}
+        </nav>
+      </div>
+    </TooltipProvider>
+  );
+}
